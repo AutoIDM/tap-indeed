e6bde05c82ac4314586a71668117a1eed4beff8c
Derek Visch <dvisch@autoidm.com>
2023-02-01 15:35:14 -0500

    Initial Employer Stats Report, and reformat

--- tap_indeedsponsoredjobs/streams.py
+++ tap_indeedsponsoredjobs/streams.py
@@ -1,18 +1,21 @@
 """Stream type classes for tap-indeedsponsoredjobs."""
 
 from pathlib import Path
-from typing import Any, Dict, Optional, Union, List, Iterable
-from singer_sdk import typing as th  # JSON Schema typing helpers
+from typing import Any, Dict, Iterable, List, Optional, Union
+
 from memoization import cached
+from singer_sdk import typing as th  # JSON Schema typing helpers
 from singer_sdk.authenticators import OAuthAuthenticator
-from tap_indeedsponsoredjobs.client import IndeedSponsoredJobsStream
+
 from tap_indeedsponsoredjobs.auth import IndeedSponsoredJobsAuthenticator
+from tap_indeedsponsoredjobs.client import IndeedSponsoredJobsStream
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
 class Employers(IndeedSponsoredJobsStream):
     """List of all employers we have access to"""
+
     name = "employers"
     path = "/appinfo"
     primary_keys = ["id"]
@@ -23,30 +26,77 @@ class Employers(IndeedSponsoredJobsStream):
     @cached
     def authenticator(self) -> IndeedSponsoredJobsAuthenticator:
         """Return a new authenticator object."""
-        return IndeedSponsoredJobsAuthenticator.create_multiemployerauth_for_stream(self)
-    
+        return IndeedSponsoredJobsAuthenticator.create_multiemployerauth_for_stream(
+            self
+        )
+
     # Optionally, you may also use `schema_filepath` in place of `schema`:
     # schema_filepath = SCHEMAS_DIR / "users.json"
     schema = th.PropertiesList(
         th.Property("name", th.StringType),
-        th.Property(
-            "id",
-            th.StringType
-        ),
+        th.Property("id", th.StringType),
         th.Property(
             "name",
             th.StringType,
         ),
     ).to_dict()
-    
+
     def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
         """Return a context dictionary for child streams."""
         return {
-                "_sdc_employer_id": record["id"],
-                }
+            "_sdc_employer_id": record["id"],
+        }
+
+
+class EmployerStatsReport(IndeedSponsoredJobsStream):
+    """Employer Stats Report.
+
+    These reports are CSVs generated in daily increments.
+    The benefit over this stream vs the Campaign Performance Stats stream is that this stream includes extra information in the report such as Job information.
+
+    Line Number is the line number in the CSV returned line 1 = headers
+    """
+
+    name = "employer_stats_report"
+    path = "/v1/campaigns/stats"
+    primary_keys = ["_sdc_employer_id", "_sdc_start_date", "_sdc_line_number"]
+    replication_key = "_sdc_start_date"
+    is_sorted = True
+    parent_stream_type = Employers
+    # We assumed version 6
+    schema = th.PropertiesList(
+        th.Property("Campaign ID", th.StringType),
+        th.Property("Advertisement", th.StringType),
+        th.Property("Job", th.StringType),
+        th.Property("Job Reference Number", th.StringType),
+        th.Property("Platform", th.StringType),
+        th.Property("Clicks", th.StringType),
+        th.Property("Impressions", th.StringType),
+        th.Property("Conversions", th.StringType),
+        th.Property("Indeed Apply Conversions", th.StringType),
+        th.Property("Spend (currency)", th.StringType),
+        th.Property("Organic Clicks", th.StringType),
+        th.Property("Organic Impressions", th.StringType),
+        th.Property("Apply Starts", th.StringType),
+        th.Property("Organic Apply Starts", th.StringType),
+        th.Property("_sdc_employer_id", th.StringType),
+        th.Property("_sdc_campaign_id", th.StringType),
+    ).to_dict()
+
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+        params: dict = {}
+        params["startDate"] = self.get_starting_replication_key_value(context)
+        params["endDate"] = self.get_starting_replication_key_value(context) + 1
+
+        return params
+
 
 class Campaigns(IndeedSponsoredJobsStream):
     """Campaigns per Employer"""
+
     name = "campaigns"
     path = "/v1/campaigns"
     primary_keys = ["Id"]
@@ -54,33 +104,37 @@ class Campaigns(IndeedSponsoredJobsStream):
     replication_key = None
     parent_stream_type = Employers
     schema = th.PropertiesList(
-            th.Property("Name", th.StringType),
-            th.Property("Id", th.StringType),
-            th.Property("Status", th.StringType),
-            th.Property("_sdc_employer_id", th.StringType),
-            ).to_dict()
+        th.Property("Name", th.StringType),
+        th.Property("Id", th.StringType),
+        th.Property("Status", th.StringType),
+        th.Property("_sdc_employer_id", th.StringType),
+    ).to_dict()
+
     def get_url_params(
         self, context: Optional[dict], next_page_token: Optional[Any]
     ) -> Dict[str, Any]:
         """Return a dictionary of values to be used in URL parameterization."""
         params: dict = {}
-        #if next_page_token:
+        # if next_page_token:
         #    params["page"] = next_page_token
-        #if self.replication_key:
+        # if self.replication_key:
         #    params["sort"] = "asc"
         #    params["order_by"] = self.replication_key
-        params["perPage"]=1000000000
-        params["status"]="Active"
+        params["perPage"] = 1000000000
+        params["status"] = "Active"
         return params
+
     def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
         """Return a context dictionary for child streams."""
         return {
-                "_sdc_employer_id": context["_sdc_employer_id"],
-                "_sdc_campaign_id": record["Id"],
-                }
+            "_sdc_employer_id": context["_sdc_employer_id"],
+            "_sdc_campaign_id": record["Id"],
+        }
+
 
 class CampaignPerformanceStats(IndeedSponsoredJobsStream):
     """Campaign Performance per Campaign"""
+
     name = "campaign_performance_stats"
     path = "/v1/campaigns/{_sdc_campaign_id}/stats"
     primary_keys = ["Id", "Date"]
@@ -89,40 +143,43 @@ class CampaignPerformanceStats(IndeedSponsoredJobsStream):
     is_sorted = False
     parent_stream_type = Campaigns
     schema = th.PropertiesList(
-            th.Property("Name", th.StringType),
-            th.Property("Id", th.StringType),
-            th.Property("Date", th.DateType),
-            th.Property("Clicks", th.IntegerType),
-            th.Property("Impressions", th.IntegerType),
-            th.Property("Conversions", th.IntegerType),
-            th.Property("CurrencyCode", th.StringType),
-            th.Property("Cost", th.NumberType),
-            th.Property("OrganicClicks", th.IntegerType),
-            th.Property("OrganicImpressions", th.IntegerType),
-            th.Property("Applystarts", th.IntegerType),
-            th.Property("OrganicApplystarts", th.IntegerType),
-            th.Property("_sdc_employer_id", th.StringType),
-            th.Property("_sdc_campaign_id", th.StringType),
-            ).to_dict()
+        th.Property("Name", th.StringType),
+        th.Property("Id", th.StringType),
+        th.Property("Date", th.DateType),
+        th.Property("Clicks", th.IntegerType),
+        th.Property("Impressions", th.IntegerType),
+        th.Property("Conversions", th.IntegerType),
+        th.Property("CurrencyCode", th.StringType),
+        th.Property("Cost", th.NumberType),
+        th.Property("OrganicClicks", th.IntegerType),
+        th.Property("OrganicImpressions", th.IntegerType),
+        th.Property("Applystarts", th.IntegerType),
+        th.Property("OrganicApplystarts", th.IntegerType),
+        th.Property("_sdc_employer_id", th.StringType),
+        th.Property("_sdc_campaign_id", th.StringType),
+    ).to_dict()
+
     def get_url_params(
         self, context: Optional[dict], next_page_token: Optional[Any]
     ) -> Dict[str, Any]:
         """Return a dictionary of values to be used in URL parameterization."""
         params: dict = {}
-        #TODO call super
-        #if next_page_token:
+        # TODO call super
+        # if next_page_token:
         #    params["page"] = next_page_token
-        #if self.replication_key:
+        # if self.replication_key:
         #    params["sort"] = "asc"
         #    params["order_by"] = self.replication_key
-        params["perPage"]=1000000000
-        #params["startDate"]=self.config["start_date"]
-        params["startDate"]=self.get_starting_replication_key_value(context)
+        params["perPage"] = 1000000000
+        # params["startDate"]=self.config["start_date"]
+        params["startDate"] = self.get_starting_replication_key_value(context)
 
         return params
 
+
 class CampaignBudget(IndeedSponsoredJobsStream):
     """Campaign Budget per Campaign"""
+
     name = "campaign_budget"
     path = "/v1/campaigns/{_sdc_campaign_id}/budget"
     primary_keys = ["_sdc_campaign_id"]
@@ -130,13 +187,15 @@ class CampaignBudget(IndeedSponsoredJobsStream):
     replication_key = None
     parent_stream_type = Campaigns
     schema = th.PropertiesList(
-            th.Property("budgetMonthlyLimit", th.NumberType),
-            th.Property("_sdc_employer_id", th.StringType),
-            th.Property("_sdc_campaign_id", th.StringType),
-            ).to_dict()
+        th.Property("budgetMonthlyLimit", th.NumberType),
+        th.Property("_sdc_employer_id", th.StringType),
+        th.Property("_sdc_campaign_id", th.StringType),
+    ).to_dict()
+
 
 class CampaignInfo(IndeedSponsoredJobsStream):
     """Campaign Info per Campaign"""
+
     name = "campaign_info"
     path = "/v1/campaigns/{_sdc_campaign_id}"
     primary_keys = ["Id"]
@@ -144,21 +203,29 @@ class CampaignInfo(IndeedSponsoredJobsStream):
     replication_key = None
     parent_stream_type = Campaigns
     schema = th.PropertiesList(
-            th.Property("Name", th.StringType),
-            th.Property("Id", th.StringType),
-            th.Property("Type", th.StringType),
-            th.Property("Status", th.StringType),
-            th.Property("CurrencyCode", th.StringType),
-            th.Property("TrackingToken", th.StringType),
-            th.Property("Objective", th.ObjectType(
-                                    th.Property("description", th.StringType),
-                                    th.Property("objectiveType", th.StringType),
-                                    )),
-            th.Property("NonSpendingReasons", th.ArrayType(th.ObjectType(
-                                    th.Property("type", th.StringType),
-                                    th.Property("description", th.StringType),
-                                                ))),
-            th.Property("SpendingChannels", th.ArrayType(th.StringType)),
-            th.Property("_sdc_employer_id", th.StringType),
-            th.Property("_sdc_campaign_id", th.StringType),
-            ).to_dict()
+        th.Property("Name", th.StringType),
+        th.Property("Id", th.StringType),
+        th.Property("Type", th.StringType),
+        th.Property("Status", th.StringType),
+        th.Property("CurrencyCode", th.StringType),
+        th.Property("TrackingToken", th.StringType),
+        th.Property(
+            "Objective",
+            th.ObjectType(
+                th.Property("description", th.StringType),
+                th.Property("objectiveType", th.StringType),
+            ),
+        ),
+        th.Property(
+            "NonSpendingReasons",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("type", th.StringType),
+                    th.Property("description", th.StringType),
+                )
+            ),
+        ),
+        th.Property("SpendingChannels", th.ArrayType(th.StringType)),
+        th.Property("_sdc_employer_id", th.StringType),
+        th.Property("_sdc_campaign_id", th.StringType),
+    ).to_dict()
